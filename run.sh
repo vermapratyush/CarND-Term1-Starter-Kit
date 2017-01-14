f4e8afa89aea6e3eb87bfbfb49db179fe3575045
Pratyush Verma <vermapratyush@gmail.com>
2017-01-15 02:30:26 +0800

    Traffic sign with Keras

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ if [ -z "$1" ]
     jupyter notebook 
 elif [ "$1" == *".ipynb"* ]
   then
-    jupyter notebook "$1"
+    THEANO_FLAGS=device=gpu,floatX=float32 jupyter notebook "$1"
 else
     exec "$@"
 fi
